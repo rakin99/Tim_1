a911dae0b2822bc7ca6139e1bb8b96b228a21253
nestor9 <golubnemanja57@gmail.com>
2020-06-19 13:23:30 +0200

    Odradjen CreateContactActivity i kreirani u meniju xml fajlovi za kontakt

--- app/src/main/java/com/example/mojprojekat/aktivnosti/CreateContactActivity.java
+++ app/src/main/java/com/example/mojprojekat/aktivnosti/CreateContactActivity.java
@@ -1,11 +1,20 @@
 package com.example.mojprojekat.aktivnosti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import com.example.mojprojekat.R;
+import com.example.mojprojekat.fragmenti.FragmentCreateContact;
+import com.example.mojprojekat.tools.FragmentTransition;
 
 public class CreateContactActivity extends AppCompatActivity {
 
@@ -13,34 +22,49 @@ public class CreateContactActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_contact);
+        this.setTitle("Kreiraj_kontakt");
+        FragmentTransition.to(FragmentCreateContact.newInstance(), this, true, R.id.createContactContent);
+
+        EditText etUsername=findViewById(R.id.etUsername);
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        etUsername.setText(sharedPreferences.getString(getString(R.string.login),"Nema ulogovanog"));
+        Toolbar tbCreateContact=findViewById(R.id.tbCreateContact);
+        setSupportActionBar(tbCreateContact);
     }
 
     @Override
-    protected void onStart(){
-        super.onStart();
-        Toast.makeText(this, "onStart()",Toast.LENGTH_SHORT).show();
-    }
-    @Override
-    protected  void onResume(){
-        super.onResume();
-        Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
     }
 
     @Override
-    protected void onPause(){
-        super.onPause();
-        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_back:
+                Intent intent = new Intent(CreateContactActivity.this, ContactsActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            case R.id.action_cancel:
+                Intent intent1 = new Intent(CreateContactActivity.this, ContactsActivity.class);
+                startActivity(intent1);
+                finish();
+                return true;
+            case R.id.action_save:
+                Toast.makeText(this, "Uspešno sacuvano!",Toast.LENGTH_SHORT).show();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
-    protected void onStop(){
-        super.onStop();
-        Toast.makeText(this,"onStop()", Toast.LENGTH_SHORT).show();
-    }
-
+    protected void onStart(){ super.onStart();}
     @Override
-    protected void onDestroy(){
-        super.onDestroy();
-        Toast.makeText(this,"onDestroy()", Toast.LENGTH_SHORT).show();
-    }
+    protected  void onResume(){ super.onResume(); }
+    @Override
+    protected void onPause(){ super.onPause(); }
+    @Override
+    protected void onStop(){ super.onStop(); }
+    @Override
+    protected void onDestroy(){ super.onDestroy(); }
 }
