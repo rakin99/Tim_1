b2ce0e63fa098c652c5510d7957f5ca8b23660a4
nestor9 <golubnemanja57@gmail.com>
2020-06-22 15:24:04 +0200

    Odradjen ContactActivity i ContactsActivity i kreirani content_contact

--- app/src/main/java/com/example/mojprojekat/aktivnosti/ContactActivity.java
+++ app/src/main/java/com/example/mojprojekat/aktivnosti/ContactActivity.java
@@ -1,49 +1,135 @@
 package com.example.mojprojekat.aktivnosti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.mojprojekat.R;
+import com.example.mojprojekat.database.DBContentProviderContact;
+import com.example.mojprojekat.fragmenti.FragmentContact;
+import com.example.mojprojekat.model.Contact;
+import com.example.mojprojekat.model.Message;
+import com.example.mojprojekat.tools.FragmentTransition;
+
+import java.text.ParseException;
+
+import static com.example.mojprojekat.tools.Data.getMessageById;
 
 public class ContactActivity extends AppCompatActivity {
+
+    private static Contact c1=new Contact(1,"Mika","Mikic","mika@gmail.com");
+    private static Contact c2=new Contact(2,"Zika","Zikic","mika@gmail.com");
+    private Uri todoUri;
+    private Message message;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contact);
+
+        FragmentTransition.to(FragmentContact.newInstance(), this, true, R.id.contactContent);
+
         Toolbar tbContact=findViewById(R.id.tbContact);
         setSupportActionBar(tbContact);
+        Bundle extras = getIntent().getExtras();
+
+        Long id = extras. getLong("id");
+        try {
+            fillData(id);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void fillData(Long id) throws ParseException {
+       /* String[] allColumns = { ReviewerSQLiteHelper.COLUMN_ID,
+                ReviewerSQLiteHelper.COLUMN_FROM, ReviewerSQLiteHelper.COLUMN_TO, ReviewerSQLiteHelper.COLUMN_CC, ReviewerSQLiteHelper.COLUMN_BCC,
+                ReviewerSQLiteHelper.COLUMN_DATE_TIME,  ReviewerSQLiteHelper.COLUMN_SUBJECT, ReviewerSQLiteHelper.COLUMN_CONTENT };
+
+        Cursor cursor = getContentResolver().query(todoUri, allColumns, null, null,
+                null);
+
+        cursor.moveToFirst();
+        Message message = createMessage(cursor);*/
+
+        todoUri = Uri.parse(DBContentProviderContact.CONTENT_URI_CONTACT + "/" + id);
+        message= getMessageById(id);
+
+        TextView tvFirstName = (TextView)findViewById(R.id.tvFirstName);
+        TextView tvLastName = (TextView)findViewById(R.id.tvLastName);
+        TextView tvEmail = (TextView)findViewById(R.id.tvEmail);
+
+        tvFirstName.setText(message.getFrom());
+        tvLastName.setText(message.getSubject());
+        tvEmail.setText(message.getContent());
+
+
+        //cursor.close();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.action_back:
+                Intent intent = new Intent(ContactActivity.this, ContactsActivity.class);
+                startActivity(intent);
+                finish();
+                return true;
+            /*case R.id.action_delete:
+                Intent intent2 = new Intent(this, MessageService.class);
+                intent2.putExtra("id",message.getId());
+                intent2.putExtra("option","delete");
+                startService(intent2);
+                message.setActive(false);
+                messages.remove(message);
+                ContentValues entryUser=new ContentValues();
+                entryUser.put(ReviewerSQLiteHelper.COLUMN_ACTIVE,message.isActive());
+                int update = getContentResolver().update(todoUri,entryUser, null, null);
+                Log.d("\nBroj azuriranih redova",String.valueOf(update)+"\n");
+                Toast.makeText(this, "Uspesno ste obrisali poruku!",Toast.LENGTH_SHORT).show();
+                Intent intent1 = new Intent(EmailActivity.this, EmailsActivity.class);
+                startActivity(intent1);
+                finish();
+                return true;*/
+            case R.id.action_save:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
     protected void onStart(){
         super.onStart();
-        Toast.makeText(this, "onStart()",Toast.LENGTH_SHORT).show();
     }
     @Override
     protected  void onResume(){
         super.onResume();
-        Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onPause(){
         super.onPause();
-        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        Toast.makeText(this,"onStop()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Toast.makeText(this,"onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
--- app/src/main/java/com/example/mojprojekat/aktivnosti/ContactsActivity.java
+++ app/src/main/java/com/example/mojprojekat/aktivnosti/ContactsActivity.java
@@ -1,45 +1,319 @@
 package com.example.mojprojekat.aktivnosti;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import com.example.mojprojekat.R;
+import com.example.mojprojekat.adapteri.DrawerListAdapter;
+import com.example.mojprojekat.fragmenti.FragmentEmails;
+import com.example.mojprojekat.model.NavItem;
+import com.example.mojprojekat.sync.SyncReceiver;
+import com.example.mojprojekat.sync.SyncService;
+import com.example.mojprojekat.tools.Data;
+import com.example.mojprojekat.tools.FragmentTransition;
+import com.example.mojprojekat.tools.ReviewerTools;
+
+import java.text.ParseException;
+import java.util.ArrayList;
+
+import static java.lang.Integer.parseInt;
+import static java.lang.System.currentTimeMillis;
+import static java.lang.System.out;
 
 public class ContactsActivity extends AppCompatActivity {
+
+    private ImageView profile;
+    private TextView profileUserName;
+    private RelativeLayout profileBox;
+    private ListView mDrawerList;
+    private DrawerLayout mDrawerLayout;
+    private ArrayList<NavItem> mNavItems = new ArrayList<NavItem>();
+    private ActionBarDrawerToggle mDrawerToggle;
+    private RelativeLayout mDrawerPane;
+    private CharSequence mTitle;
+    private AlertDialog dialog;
+
+    public static String SYNC_DATA = "SYNC_DATA";
+    private SyncReceiver sync;
+
+    private PendingIntent pendingIntent;
+
+    private AlarmManager manager;
+
+    private boolean allowSync;
+
+    private String sort;
+    private SharedPreferences sharedPreferences;
+    private String synctime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+        prepareMenu(mNavItems);
+
+        mTitle=getTitle();
+        mDrawerLayout=findViewById(R.id.drawerLayout);
+        mDrawerList = findViewById(R.id.navList);
+        profile=findViewById(R.id.avatar);
+        profileUserName=findViewById(R.id.userName);
+
+        profileBox=findViewById(R.id.profileBox);
+
+        mDrawerPane=findViewById(R.id.drawerPane);
+        DrawerListAdapter adapter=new DrawerListAdapter(this,mNavItems);
+
+        // postavljamo senku koja preklama glavni sadrzaj
+        mDrawerLayout.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+        // dodajemo listener koji ce reagovati na klik pojedinacnog elementa u listi
+        mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+        profileBox.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(ContactsActivity.this, ProfileActivity.class);
+                startActivity(intent);
+            }
+        });
+        // drawer-u postavljamo unapred definisan adapter
+        mDrawerList.setAdapter(adapter);
+
+        Toolbar tbContacts=findViewById(R.id.tbContacts);
+        setSupportActionBar(tbContacts);
+
+        final ActionBar actionBar = getSupportActionBar();
+
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setHomeAsUpIndicator(R.drawable.drawer);
+        }
+
+        /*
+         * drawer-u specificiramo za koju referencu toolbar-a da se veze
+         * Specificiramo sta ce da pise unutar Toolbar-a kada se drawer otvori/zatvori
+         * i specificiramo sta ce da se desava kada se drawer otvori/zatvori.
+         * */
+        mDrawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                mDrawerLayout,         /* DrawerLayout object */
+                tbContacts,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                getSupportActionBar().setTitle("iReviewer");
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // Izborom na neki element iz liste, pokrecemo akciju
+        if (savedInstanceState == null) {
+
+            selectItemFromDrawer(0);
+        }
+
+        setUpReceiver();
+        consultPreferences();
     }
+
+    private class DrawerItemClickListener implements ListView.OnItemClickListener {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            selectItemFromDrawer(position);
+        }
+    }
+
+    private void selectItemFromDrawer(int position) {
+        if(position == 0){
+            FragmentTransition.to(FragmentEmails.newInstance(), this, false, R.id.mainContent);
+        }else if(position == 1){
+            Intent intent = new Intent(ContactsActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        }else if(position == 2){
+            Intent intent = new Intent(ContactsActivity.this, FoldersActivity.class);
+            startActivity(intent);
+        }else if(position == 3){
+            sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+            SharedPreferences.Editor editor=sharedPreferences.edit();
+            editor.putString(getString(R.string.login),"");
+            editor.commit();
+            Intent intent = new Intent(ContactsActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }else{
+            Log.e("DRAWER", "Nesto van opsega!");
+        }
+
+        mDrawerList.setItemChecked(position, true);
+        if(position != 2)
+        {
+            setTitle(mNavItems.get(position).getmTitle());
+        }
+        mDrawerLayout.closeDrawer(mDrawerPane);
+    }
+
     @Override
-    protected void onStart(){
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void prepareMenu(ArrayList<NavItem> mNavItems){
+        mNavItems.add(new NavItem(getString(R.string.poruke),R.drawable.email));
+        mNavItems.add(new NavItem(getString(R.string.settings),R.drawable.ic_settings_applications_black_24dp));
+        mNavItems.add(new NavItem(getString(R.string.all_folders),R.drawable.folders));
+        mNavItems.add(new NavItem(getString(R.string.logout),R.drawable.exit));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_new:{
+                Intent intent = new Intent(ContactsActivity.this, CreateContactActivity.class);
+                startActivity(intent);
+            }
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void setTitle(CharSequence title) {
+        mTitle = title;
+        getSupportActionBar().setTitle(mTitle);
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
+
+    @Override
+    protected void onStart() {
         super.onStart();
-        Toast.makeText(this, "onStart()",Toast.LENGTH_SHORT).show();
+    }
+    private void setUpReceiver(){
+        sync = new SyncReceiver();
+
+        // Retrieve a PendingIntent that will perform a broadcast
+        Intent alarmIntent = new Intent(this, SyncService.class);
+        pendingIntent = PendingIntent.getService(this, 0, alarmIntent, 0);
+        manager = (AlarmManager)getSystemService(Context.ALARM_SERVICE);
     }
     @Override
     protected  void onResume(){
         super.onResume();
-        Toast.makeText(this, "onResume()",Toast.LENGTH_SHORT).show();
+        try {
+            Data.readMessages(this,sort);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+
+        out.println("Broj poruka: "+ Data.messages.size() +"<---------------------------------------------");
+
+        //Za slucaj da referenca nije postavljena da se izbegne problem sa androidom!
+        if (manager == null) {
+            setUpReceiver();
+        }
+
+        if(allowSync){
+            int interval = ReviewerTools.calculateTimeTillNextSync(parseInt(synctime));
+            manager.setRepeating(AlarmManager.RTC_WAKEUP, currentTimeMillis(), interval, pendingIntent);
+            /*Intent alarmIntent = new Intent(this, SyncService.class);
+            startService(alarmIntent);*/
+        }
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(SYNC_DATA);
+
+        registerReceiver(sync, filter);
+    }
+
+    private void consultPreferences(){
+
+        /*
+         * getDefaultSharedPreferences():
+         * koristi podrazumevano ime preference-file-a.
+         * Podrzazumevani fajl je setovan na nivou aplikacije tako da sve aktivnosti u istom context-u mogu da mu pristupe jednostavnije
+         * getSharedPreferences(name,mode):
+         * trazi da se specificira ime preference file-a requires i mod u kome se radi (e.g. private, world_readable, etc.)
+         */
+        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+
+        /*
+         * Koristeci parove kljuc-vrednost iz shared preferences mozemo da dobijemo
+         * odnosno da zapisemo nekakve vrednosti. Te vrednosti mogu da budu iskljucivo
+         * prosti tipovi u Javi.
+         * Kao prvi parametar prosledjujemo kljuc, a kao drugi podrazumevanu vrednost,
+         * ako nesto pod tim kljucem se ne nalazi u storage-u, da dobijemo podrazumevanu
+         * vrednost nazad, i to nam je signal da nista nije sacuvano pod tim kljucem.
+         * */
+        synctime = sharedPreferences.getString(getString(R.string.pref_sync_list), "1");
+        System.out.println("\nsynctime: "+synctime+"<-----------------------------------\n");// pola minuta
+        allowSync = sharedPreferences.getBoolean(getString(R.string.pref_sync), true);
+        sort = sharedPreferences.getString(getString(R.string.sort), "DESC");
+        System.out.println("\nsort: "+sort+"<-----------------------------------\n");
     }
 
     @Override
     protected void onPause(){
+        if (manager != null) {
+            manager.cancel(pendingIntent);
+        }
+
+        if(sync != null){
+            unregisterReceiver(sync);
+        }
+
         super.onPause();
-        Toast.makeText(this, "onPause()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onStop(){
         super.onStop();
-        Toast.makeText(this,"onStop()", Toast.LENGTH_SHORT).show();
     }
 
     @Override
     protected void onDestroy(){
         super.onDestroy();
-        Toast.makeText(this,"onDestroy()", Toast.LENGTH_SHORT).show();
     }
 }
